850b14cad4e2b795a8d0ace86aadc4e25286905d
Alex Verkhaturau <Alex.Verkhaturau@kaseya.com>
2020-01-31 23:04:52 +0300

    fix concurrency issue, added working status output

--- DependenciesSearcher.cpp
+++ DependenciesSearcher.cpp
@@ -1,4 +1,5 @@
 #include "INIReader.h"
+#include <array>
 #include <iostream>
 #include <filesystem>
 #include <list>
@@ -21,15 +22,15 @@ struct Params
 };
 
 
-Params FetchParameters( INIReader& iniReader );
+Params FetchParameters(INIReader& iniReader);
 
-std::list<path> FilterFilesByExtentions( const std::list<path>& sourceDirs, const std::regex& extentionsPattern );
+std::list<path> FilterFilesByExtentions(const std::list<path>& sourceDirs, const std::regex& extentionsPattern);
 
-std::unordered_map<std::string, std::vector<path>> DetectDependencies( const std::list<path>& searched, const std::list<path>& scanned );
+std::unordered_map<std::string, std::vector<path>> DetectDependencies(const std::list<path>& searched, const std::list<path>& scanned);
 
 
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     // Проверка аргументов
     if (argc > 2) {
@@ -37,7 +38,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    INIReader ini( (argc == 2) ? argv[1] : "config.ini" );
+    INIReader ini((argc == 2) ? argv[1] : "config.ini");
 
     if (ini.ParseError() < 0)
     {
@@ -46,17 +47,31 @@ int main(int argc, char **argv)
     }
 
     // Выдираем параметры из .ini-файла
-    Params params = FetchParameters( ini );
+    Params params = FetchParameters(ini);
 
     // Формируем списки искомых и сканируемых файлов
-    std::list<path> searched_FileNames = FilterFilesByExtentions( params.searchedDirs, params.searched_extentions_pattern);
-    std::list<path> scanned_FileNames = FilterFilesByExtentions( params.scannedDirs, params.scanned_extentions_pattern );
+    std::list<path> searched_FileNames = FilterFilesByExtentions(params.searchedDirs, params.searched_extentions_pattern);
+    std::list<path> scanned_FileNames = FilterFilesByExtentions(params.scannedDirs, params.scanned_extentions_pattern);
+
+    std::cout << "Searching dependencies of ";
+    for (const auto& dir : params.searchedDirs)
+        std::cout << dir << " ";
+    std::cout << " in ";
+    for (const auto& dir : params.scannedDirs)
+        std::cout << dir << " ";
+    std::cout << " (" << scanned_FileNames.size() << " files)\n";
+
+    const auto start = std::chrono::steady_clock::now();
 
     // Сканируем файлы на присутствие имён искомых файлов
-    std::unordered_map<std::string, std::vector<path>> potentialDependencies = DetectDependencies( searched_FileNames, scanned_FileNames );
+    std::unordered_map<std::string, std::vector<path>> potentialDependencies = DetectDependencies(searched_FileNames, scanned_FileNames);
 
+    const auto finish = std::chrono::steady_clock::now();
+    std::cout << "\nWorked " << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << "ms\n";
+
+    std::cout << "Writing results to dependencies.txt\n";
     // Выводим лог зависимостей
-    std::ofstream results( "dependencies.txt" );
+    std::ofstream results("dependencies.txt");
     for (const auto& dep : potentialDependencies)
     {
         results << "Name of the file \"" << dep.first << "\" is present in file(s):";
@@ -66,20 +81,20 @@ int main(int argc, char **argv)
         }
         results << std::endl;
     }
-    results.close();
+
     return 0;
 }
 
 
 
-Params FetchParameters( INIReader& iniReader )
+Params FetchParameters(INIReader& iniReader)
 {
     Params params;
-    params.searchedDirs = iniReader.GetPathList( "PATHS", "Searched" );
-    params.scannedDirs = iniReader.GetPathList( "PATHS", "Scanned" );
+    params.searchedDirs = iniReader.GetPathList("PATHS", "Searched");
+    params.scannedDirs = iniReader.GetPathList("PATHS", "Scanned");
 
-    std::list<std::string> searched_extentions = iniReader.GetStringList( "EXTENTIONS", "Searched" );
-    std::list<std::string> scanned_extentions = iniReader.GetStringList( "EXTENTIONS", "Scanned" );
+    std::list<std::string> searched_extentions = iniReader.GetStringList("EXTENTIONS", "Searched");
+    std::list<std::string> scanned_extentions = iniReader.GetStringList("EXTENTIONS", "Scanned");
 
     std::string searched_regStr = "(";
     std::string scanned_regStr = "(";
@@ -93,33 +108,33 @@ Params FetchParameters( INIReader& iniReader )
     searched_regStr.back() = ')';
     scanned_regStr.back() = ')';
 
-    params.searched_extentions_pattern = std::regex( searched_regStr );
-    params.scanned_extentions_pattern = std::regex( scanned_regStr );
+    params.searched_extentions_pattern = std::regex(searched_regStr);
+    params.scanned_extentions_pattern = std::regex(scanned_regStr);
 
     return params;
 }
 
 
-std::list<path> FilterFilesByExtentions( const std::list<path>& sourceDirs, const std::regex& extentionsPattern )
+std::list<path> FilterFilesByExtentions(const std::list<path>& sourceDirs, const std::regex& extentionsPattern)
 {
     std::list<path> filtered_files;
     for (const auto& sourceDir : sourceDirs)
     {
-        if (!std::filesystem::exists( sourceDir ))
+        if (!std::filesystem::exists(sourceDir))
         {
             std::cout << "Path doesn't exist:\n\t" << sourceDir << "\n";
             continue;
         }
 
-        recursive_directory_iterator source_it( sourceDir );
+        recursive_directory_iterator source_it(sourceDir);
         for (; source_it != recursive_directory_iterator(); ++source_it)
         {
             if (source_it->is_directory())
                 continue;
 
-            if (std::regex_match( source_it->path().extension().string(), extentionsPattern ))
+            if (std::regex_match(source_it->path().extension().string(), extentionsPattern))
             {
-                filtered_files.push_back( source_it->path() );
+                filtered_files.push_back(source_it->path());
             }
         }
     }
@@ -131,52 +146,61 @@ std::list<path> FilterFilesByExtentions( const std::list<path>& sourceDirs, cons
 //                          key: искомый файл,
 //                          value: список файлов, в которых присутствует его имя
 //                         >
-std::unordered_map<std::string, std::vector<path>> DetectDependencies( const std::list<path>& searched_FileNames, const std::list<path>& scanned_FileNames )
+std::unordered_map<std::string, std::vector<path>> DetectDependencies(const std::list<path>& searched_FileNames, const std::list<path>& scanned_FileNames)
 {
-    std::vector<std::future<void>> todo;
-    todo.reserve( scanned_FileNames.size() );
-    std::ifstream scanned_File;
-    std::mutex mut_writeDependency;
-    std::mutex mut_openScannedFile;
     std::unordered_map<std::string, std::vector<path>> potentialDependencies;
-    //size_t i = 0;
 
-    for (const auto& scanned_FileName : scanned_FileNames)
+    std::cout << "[0%] preparing...\r";
     {
-        todo.push_back( std::async( [&scanned_FileName, &scanned_File, &searched_FileNames, &mut_writeDependency, &mut_openScannedFile, &potentialDependencies/*, &i*/]()
-        {
-            std::string scanned_FileContent;
-            {
-                std::lock_guard<std::mutex> lock( mut_openScannedFile );
-                //std::cout << i++ << ": working in thread " << std::this_thread::get_id() << std::endl;
+        std::mutex mut_writeDependency;
+        std::vector<std::future<void>> todo;
+        todo.reserve(scanned_FileNames.size());
 
-                scanned_File.open( scanned_FileName );
-
-                if (!scanned_File.is_open()) {
-                    return;
-                }
-                scanned_FileContent = std::string( (std::istreambuf_iterator<char>( scanned_File )), std::istreambuf_iterator<char>() );
-                scanned_File.close();
-            }
-
-            for (const auto& searched_FileName : searched_FileNames)
-            {
-                std::regex pattern_toSearch( searched_FileName.filename().replace_extension().string() );
-                if (std::regex_search( scanned_FileContent, pattern_toSearch ))
+        size_t i = 0;
+        for (const auto& scanned_FileName : scanned_FileNames) {
+            if (++i % ((scanned_FileNames.size()/5)+1) == 0)
+                std::cout << "[" << i * 5 / scanned_FileNames.size() << "%] searching...  \r";
+            todo.push_back(std::async([scanned_FileName, &searched_FileNames, &mut_writeDependency, &potentialDependencies]()
                 {
+                    std::string scanned_FileContent;
+                    {
+                        std::ifstream scanned_File(scanned_FileName);
 
-                    std::lock_guard<std::mutex> lock( mut_writeDependency );
-                    potentialDependencies[searched_FileName.generic_string()].push_back( scanned_FileName );
-                }
+                        if (!scanned_File.is_open()) {
+                            std::cout << "Cannot open " << scanned_FileName << "\n";
+                            return;
+                        }
+                        scanned_FileContent = std::string((std::istreambuf_iterator<char>(scanned_File)), std::istreambuf_iterator<char>());
+                    }
+
+                    for (const auto& searched_FileName : searched_FileNames)
+                    {
+                        std::regex pattern_toSearch(searched_FileName.filename().string());
+                        if (std::regex_search(scanned_FileContent, pattern_toSearch))
+                        {
+
+                            std::lock_guard<std::mutex> lock(mut_writeDependency);
+                            potentialDependencies[searched_FileName.generic_string()].push_back(scanned_FileName);
+                        }
+                    }
+                }));
+        }
+
+        std::cout << "[5%] searching...\r";
+
+        // reporting percentage
+        for (size_t finished =0; finished < scanned_FileNames.size()*19/20;) {
+            finished = 0;
+            for (auto& f : todo) {
+                if (f.wait_for(std::chrono::nanoseconds(1)) == std::future_status::ready)
+                    ++finished;
             }
-
-            return;
-        }));
+            std::cout << "[" << 5 + finished * 94 / scanned_FileNames.size() << "%] searching...      \r";
+            std::this_thread::sleep_for(std::chrono::seconds(2));
+        }
+        std::cout << "[98%] searching...        \r";
     }
-
-    for (const auto& future : todo) {
-        future.wait();
-    }
+    std::cout << "[100%] done.             \n";
 
     return potentialDependencies;
 }
